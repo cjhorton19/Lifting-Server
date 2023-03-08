73403c4f66dc6c8aca5b19203f9131753ecfcba6
web3fren <officialweb3fren@gmail.com>
2023-03-08 11:47:43 -0800

    Updated spacing

--- server.js
+++ server.js
@@ -49,82 +49,82 @@ app.post('/createUser', async (req, res) => {
 });
 
 app.post('/createExercise', async (req, res) => {
-console.log(req.body);
-const {muscleGroup, exercise, reps, weight} = req.body;
-const query = `INSERT INTO exercises (date, muscle_group, exercise, reps, weight, user_id)
-VALUES ('${getFormattedDate()}', '${muscleGroup}', '${exercise}', ${reps}, ${weight}, 1);`;
-console.log(query);
-excecuteQuery(query, res);
+  console.log(req.body);
+  const {muscleGroup, exercise, reps, weight} = req.body;
+  const query = `INSERT INTO exercises (date, muscle_group, exercise, reps, weight, user_id)
+  VALUES ('${getFormattedDate()}', '${muscleGroup}', '${exercise}', ${reps}, ${weight}, 1);`;
+  console.log(query);
+  excecuteQuery(query, res);
 });
 
 app.post('/createExerciseId', async (req, res) => {
-console.log(req.body);
-const {exercise} = req.body;
-const query = {
-  text: `
-    INSERT INTO exercises (exercise, exercise_id)
-    VALUES ($1, COALESCE((SELECT MAX(exercise_id) FROM exercises), 0) + 1)
-    ON CONFLICT (exercise) DO UPDATE
-    SET exercise = excluded.exercise
-    RETURNING exercise_id
-  `,
-  values: [exercise],
-};
-console.log(query);
-excecuteQuery(query, res);
+  console.log(req.body);
+  const {exercise} = req.body;
+  const query = {
+    text: `
+      INSERT INTO exercises (exercise, exercise_id)
+      VALUES ($1, COALESCE((SELECT MAX(exercise_id) FROM exercises), 0) + 1)
+      ON CONFLICT (exercise) DO UPDATE
+      SET exercise = excluded.exercise
+      RETURNING exercise_id
+    `,
+    values: [exercise],
+  };
+  console.log(query);
+  excecuteQuery(query, res);
 });
 
 app.post('/createExercises', async (req, res) => {
-const {exercises} = req.body;
-let values = '';
-for (let i = 0; i < exercises.length; i++) {
-  const exercise = exercises[i];
-  values += `('${getFormattedDate()}', '${exercise.muscleGroup}', '${exercise.exercise}', ${exercise.reps}, ${exercise.weight}, 1)`;
-  
-  if (i !== exercises.length - 1) {
-    values += ', ';
+  const {exercises} = req.body;
+  let values = '';
+  for (let i = 0; i < exercises.length; i++) {
+    const exercise = exercises[i];
+    values += `('${getFormattedDate()}', '${exercise.muscleGroup}', '${exercise.exercise}', ${exercise.reps}, ${exercise.weight}, 1)`;
+    
+    if (i !== exercises.length - 1) {
+      values += ', ';
+    }
   }
-}
-const query = `INSERT INTO exercises (date, muscle_group, exercise, reps, weight, user_id)
-VALUES ${values};`;
-console.log(query);
-excecuteQuery(query, res);
+  const query = `INSERT INTO exercises (date, muscle_group, exercise, reps, weight, user_id)
+  VALUES ${values};`;
+  console.log(query);
+  excecuteQuery(query, res);
 });
 
 //Below is the read server
 app.get('/autocomplete', async (req, res) => {
-const { input } = req.query;
-const query = {
-  text: 'SELECT DISTINCT exercise, exercise_id FROM exercises WHERE exercise ILIKE $1 ORDER BY exercise LIMIT 10',
-  values: [`%${input}%`],
-};
-console.log(query);
-excecuteQuery(query, res);
-console.log(query);
+  const { input } = req.query;
+  const query = {
+    text: 'SELECT DISTINCT exercise, exercise_id FROM exercises WHERE exercise ILIKE $1 ORDER BY exercise LIMIT 10',
+    values: [`%${input}%`],
+  };
+  console.log(query);
+  excecuteQuery(query, res);
+  console.log(query);
 });
 
 app.post('/getUserById', async (req, res) => {
-console.log(req.body);
-const {userId} = req.body;
-const query = `SELECT * FROM users WHERE user_id = '${userId}';`;
-console.log(query);
-excecuteQuery(query, res);
+  console.log(req.body);
+  const {userId} = req.body;
+  const query = `SELECT * FROM users WHERE user_id = '${userId}';`;
+  console.log(query);
+  excecuteQuery(query, res);
 });
 
 app.post('/getUserByFirstName', async (req, res) => {
-console.log(req.body);
-const {firstName} = req.body;
-const query = `SELECT * FROM users WHERE firstname = '${firstName}';`;
-console.log(query);
-excecuteQuery(query, res);
+  console.log(req.body);
+  const {firstName} = req.body;
+  const query = `SELECT * FROM users WHERE firstname = '${firstName}';`;
+  console.log(query);
+  excecuteQuery(query, res);
 });
 
 app.post('/getExercises', async (req, res) => {
-console.log(req.body);
-const {muscleGroup} = req.body;
-const query = `SELECT * FROM exercises WHERE muscle_group = '${muscleGroup}';`;
-console.log(query);
-excecuteQuery(query, res);
+  console.log(req.body);
+  const {muscleGroup} = req.body;
+  const query = `SELECT * FROM exercises WHERE muscle_group = '${muscleGroup}';`;
+  console.log(query);
+  excecuteQuery(query, res);
 });
 
 app.listen(port, () => {
